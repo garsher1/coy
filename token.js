0b23cc4649d9ffc767a8b885bd30508234c6ca44
garsher1 <romir1461@gmail.com>
2026-06-03 16:52:39 +0700

    Add token

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "8774056291",
-    "8933309989:AAG7quBuzymImohbcJkL63iMMNNfcLqwHIs"
+    "8933309989:AAG7quBuzymImohbcJkL63iMMNNfcLqwHIs",
+    "8860367953:AAFsOMt9CyY84nMcycS4Xn3sQbhR1PrrjsI"
   ]
 }
