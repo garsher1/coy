a0af36fafb69040975684033457bd39f3cd7312f
garsher1 <romir1461@gmail.com>
2026-06-20 13:40:12 +0700

    Update tokens

--- token.js
+++ token.js
@@ -1,6 +1,5 @@
 {
   "tokens": [
-    "8774056291",
     "8933309989:AAG7quBuzymImohbcJkL63iMMNNfcLqwHIs",
     "8860367953:AAFsOMt9CyY84nMcycS4Xn3sQbhR1PrrjsI",
     "8906650740:AAGoiv93w-1S5PLy7C7DannKYuqztuq37Dg",
